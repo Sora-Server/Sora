113d0ac5897404efeaf1a50bbb43c683af9c6571
BlakJack <BlakJack@users.noreply.github.com>
2014-04-05 23:45:50 +0530

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -7,7 +7,13 @@
  */
 
 var trainerCards = {
-
+	arsh: 'blakjack',
+	arshmalik: 'blakjack',
+	blakjack: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center><img src="http://i.imgur.com/otDPUQU.png"><br><img src="http://i.imgur.com/Wdthjon.png"><img src="http://i.imgur.com/dck9vdP.png"><img src="http://i.imgur.com/5VqH7tF.png"><br><font color="brown"><blink>Ace: Swellow</blink><br><font color="brown">Haters Gonna Hate, Potatotes Gonna Potate But Blueplay\'s Gonna Masturbate');
+	},
+};
 
 Object.merge(CommandParser.commands, trainerCards);
 exports.trainerCards = trainerCards;

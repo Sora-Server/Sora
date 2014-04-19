bb1d209ba45f33823b68762463242aaca0355c3d
ChampionOnyx <shisien_0928@live.com>
2014-04-19 12:37:29 +0800

    Create moves.js

---
+++ mods/priomon/moves.js
@@ -0,0 +1,17 @@
+exports.BattleMovedex = {
+              "flameshot": {
+		num: 2000,
+		accuracy: 100,
+		basePower: 40,
+		category: "Physical",
+		desc: "Deals damage to one adjacent target. Priority +1.",
+		shortDesc: "Usually goes first.",
+		id: "Flame Shot",
+		isViable: true,
+		name: "Flame Shot",
+		pp: 30,
+		priority: 1,
+		secondary: false,
+		target: "normal",
+		type: "Fire"
+	},

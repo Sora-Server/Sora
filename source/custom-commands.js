cc92f115c757694933c6f5cc0c5ac688244db1ef
Loln8or <ancestraldog@hotmail.com>
2014-05-01 18:47:13 +1000

    Update custom-commands.js

--- source/custom-commands.js
+++ source/custom-commands.js
@@ -642,7 +642,8 @@ var customCommands = {
 	        '112.207.89.115<br />' +
 	        '69.171.166.93 - Efficient<br />'+
 	        '64.21.211.34 - Psychic kid<br />'+
-	        '217.123.61.20 - Sjado<br />');
+	        '217.123.61.20 - Sjado<br />'+
+	        ' 96.255.1.236');
         },
 
 	battlefrontier: function(target, room, user) {

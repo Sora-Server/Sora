0382fdf8884c52c30f95c2b21b993d4517bd2175
Loln8or <ancestraldog@hotmail.com>
2014-06-16 19:30:03 +1000

    Update config.js

--- config/config.js
+++ config/config.js
@@ -182,7 +182,7 @@ exports.customAvatars = {
 	  'siiilver': '059.png',
 	  'frontierquirix': '061.gif',
 	  'e4zoro': '062.gif',
-	  'gymtrnrsalty': '063.png',
+	  'gymldrsaltyy': '063.png',
 	  'pkkaiser': '064.gif',
 	  'gymldrbean877': '066.gif',
 	  'stephan4ubers': '068.gif',

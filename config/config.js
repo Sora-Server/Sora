6cdc2114619a85b5be9e7e30548075485deebc9f
Loln8or <ancestraldog@hotmail.com>
2014-07-06 23:39:20 +1000

    Update config.js

--- config/config.js
+++ config/config.js
@@ -171,7 +171,7 @@ exports.customAvatars = {
 	  'chamintst': '044.jpg',
 	  'gymldrsrewop': '048.jpg',
 	  'theone2500': '049.gif',
-	  'e4cocoa': '074.jpg',
+	  'e4cocoa': '054.png',
 	  'gymtrnrsteve': '051.gif',
 	  'reirdkrmory': '052.gif',
 	  'prophetabraham': '056.gif',

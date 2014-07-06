8fb87c1994a2a5f9d8532735f242de3de12be0f3
Loln8or <ancestraldog@hotmail.com>
2014-07-06 18:04:13 +1000

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -446,10 +446,12 @@ var trainerCards = {
         
         gasp: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('Trainer <b>gasp</b><br />'+
+		this.sendReplyBox('Frontier <b>gasp</b><br />'+
 			'<i>"Lights out."</i> <br />'+
-			'<b>Ace:</b> Sableye<br />'+
+			'<b>Ace:</b> Mega Gengar<br />'+
 			'<b>Honours:</b> Sora\'s first challenger to reach Hall of Fame.<br />'+
+			'<b>Battle rules:</b>'+
+			'-Balanced Hackmons <br/>'
 		 	'<img src="http://pldh.net/media/pokemon/gen5/blackwhite_animated_front/302.gif"> <img src="http://media.tumblr.com/tumblr_m6ci5tQsEv1qf6fp2.gif">');
         },
         

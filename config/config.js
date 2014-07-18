3589f9522af992120a6b773e766a3c9aca06d853
ChampionOnyx <shisien_0928@live.com>
2014-07-18 22:23:48 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -160,7 +160,7 @@ exports.customAvatars = {
 	  'hooh': '022.gif',
 	  'gymldrzoro': '036.gif',
 	  'frntiernight': '024.png',
-	  'gymldrarthurzh': '039.gif',
+	  'arthurzh': '039.gif',
 	  'frntirakash': '027.gif',
 	  'gymldriris': '028.png',
 	  'frntirtempest': '038.png',

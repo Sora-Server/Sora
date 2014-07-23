9235ebdb34b2971d49d77b970ff5349822d8b9c7
Loln8or <ancestraldog@hotmail.com>
2014-07-23 20:35:29 +1000

    Update config.js

--- config/config.js
+++ config/config.js
@@ -186,7 +186,8 @@ exports.customAvatars = {
 	  'stephan4ubers': '068.gif',
 	  'datslapzme': '069.gif',
 	  'amtesla': '070.gif',
-	  'gymldrterror': '072.gif', 
+	  'gymldrterror': '072.gif',
+	  'tsunabankai': '075.gif',
 	
 };
 

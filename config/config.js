248feac48737c804cc7f0f535629c2c2fdb7e617
Loln8or <ancestraldog@hotmail.com>
2014-08-07 18:30:28 +1000

    Update config.js

--- config/config.js
+++ config/config.js
@@ -187,7 +187,7 @@ exports.customAvatars = {
 	  'datslapzme': '069.gif',
 	  'amtesla': '070.gif',
 	  'gymldrterror': '072.gif',
-	  'tsunabankai': '076.gif',
+	  'gymldrtsuna': '076.gif',
 	  'frontieriggy': '077.jpg',
 	
 };

5f95e34b4c99e706f27d35e3fa59d76f250e3022
BlakJack <BlakJack@users.noreply.github.com>
2014-09-01 15:03:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -339,6 +339,12 @@ global.toName = function (name) {
 	return name;
 };
 
+global.sanitize = function(str, strEscape) {
+	str = (''+(str||''));
+	str = str.escapeHTML();
+	if (strEscape) str = str.replace(/'/g, '\\\'');
+	return str;
+};
 /**
  * Safely ensures the passed variable is a string
  * Simply doing '' + str can crash if str.toString crashes or isn't a function

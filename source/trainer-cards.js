02cd47c3cf7456e675c42aa4c0ad201b8ef97515
Loln8or <ancestraldog@hotmail.com>
2014-09-10 19:39:37 +1000

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -253,7 +253,7 @@ var trainerCards = {
         kingx: function(target, room, user) {
 		    if (!this.canBroadcast()) return;
 			this.sendReplyBox('Gym Ldr <b>KingX</b><br />'+
-			  'Leader Ranking: <font color =480087>10th</font></b> <br />'+
+			  '<b>Leader Ranking: <font color =480087>10th</font></b> <br />'+
 			  '<i>"Long live the King!"</i> <br />'+
 			  '<b>Type: <font color = 15012b>Dark</font></b><br />'+
 			  '<b>Ace:</b> Sableye<br />');

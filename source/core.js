a41a0cccdb46fffd89755675416ca66bacb917cb
BlakJack <BlakJack@users.noreply.github.com>
2014-09-20 19:49:01 +0530

    Update core.js

--- source/core.js
+++ source/core.js
@@ -321,14 +321,14 @@ var core = exports.core = {
 
 
     tournaments: {
-        tourSize: 8,
+        tourSize: 4,
         amountEarn: 10,
         winningElo: 50,
         runnerUpElo: 25,
         earningMoney: function () {
-            if (this.amountEarn === 10) return '<u>Standard (8 players = 1 buck)</u> Double (4 players = 1 buck) Quadruple (2 players = 1 bucks)';
-            if (this.amountEarn === 4) return 'Standard (8 players = 1 buck) <u>Double (4 players = 1 buck)</u> Quadruple (2 players = 1 bucks)';
-            if (this.amountEarn === 2) return 'Standard (8 players = 1 buck) Double (4 players = 1 buck) <u>Quadruple (2 players = 1 bucks)</u>';
+            if (this.amountEarn === 10) return '<u>Standard (4 players = 1 buck)</u> Double (4 players = 1 buck) Quadruple (2 players = 1 bucks)';
+            if (this.amountEarn === 4) return 'Standard (4 players = 1 buck) <u>Double (4 players = 1 buck)</u> Quadruple (2 players = 1 bucks)';
+            if (this.amountEarn === 2) return 'Standard (4 players = 1 buck) Double (4 players = 1 buck) <u>Quadruple (2 players = 1 bucks)</u>';
         }
     },
 

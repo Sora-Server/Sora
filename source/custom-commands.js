0bc05a5801822fd8a41dade14e17db9493f08d1c
SilverTactic <brahadeeshk@gmail.com>
2014-09-26 19:21:14 +0530

    Updated /sprite

--- source/custom-commands.js
+++ source/custom-commands.js
@@ -20,41 +20,63 @@ var customCommands = {
     model: 'sprite',
     sprite: function(target, room, user) {
         if (!this.canBroadcast()) return;
-		var targets = target.split(',');
-			target = targets[0];
-				target1 = targets[1];
-                if (target.toLowerCase().indexOf(' ') !== -1) {
-                target.toLowerCase().replace(/ /g,'-');
+        var targets = target.split(',');
+        target = targets[0].trim();
+        target1 = targets[1];
+        if (!toId(target)) return this.sendReply("/sprite [Pokémon], [shiny/back] - Shows the animated model of the specified Pokémon.");
+        var clean = target.toLowerCase();
+        if (target.toLowerCase().indexOf(' ') !== -1) {
+            target = target.toLowerCase().replace(/ /g, '-');
         }
-        if (target.toLowerCase().length < 4) {
-        return this.sendReply('Model not found.');
+        //this won't interfere with the generation of yanmega's sprite, since it doesn't have any alternate animations.
+        //It however DOES somewhat interfere with porygon2. So we're gonna make that one an exception.
+        if (target.indexOf('mega') == -1 && toId(target) != 'porygon2') { 
+            if (target.lastIndexOf('-') > -1) {
+                for (var i = 0; i <= target.lastIndexOf('-'); i++) {
+                    var a = target.substring(0, target.lastIndexOf('-')).replace(/-/g, ' ');
+                    break;
+                }
+            }
         }
-		var numbers = ['1','2','3','4','5','6','7','8','9','0'];
-		for (var i = 0; i < numbers.length; i++) {
-		if (target.toLowerCase().indexOf(numbers) == -1 && target.toLowerCase() !== 'porygon2') {
-        
-        
-		
-		if (target && !target1) {
-        return this.sendReply('|html|<img src = "http://www.pkparaiso.com/imagenes/xy/sprites/animados/'+target.toLowerCase().trim().replace(/ /g,'-')+'.gif">');
+
+        var correction = a ? Tools.dataSearch(a) : Tools.dataSearch(target);
+        if (correction && correction.length) {
+            for (var i = 0; i < correction.length; ++i) {
+                if (correction[i].id !== target && !i) {
+                    target = a ? target.replace(a, correction[0].id) : correction[0].name.toLowerCase();
+                }
+            }
+        } else {
+            return this.sendReply((a || clean) + ' is not a valid Pokémon.');
         }
-	if (toId(target1) == 'back' || toId(target1) == 'shiny' || toId(target1) == 'front') {
-		if (target && toId(target1) == 'back') {
-        return this.sendReply('|html|<img src = "http://play.pokemonshowdown.com/sprites/xyani-back/'+target.toLowerCase().trim().replace(/ /g,'-')+'.gif">');
-		}
-		if (target && toId(target1) == 'shiny') {
-        return this.sendReply('|html|<img src = "http://play.pokemonshowdown.com/sprites/xyani-shiny/'+target.toLowerCase().trim().replace(/ /g,'-')+'.gif">');
-		}
-		if (target && toId(target1) == 'front') {
-        return this.sendReply('|html|<img src = "http://www.pkparaiso.com/imagenes/xy/sprites/animados/'+target.toLowerCase().trim().replace(/ /g,'-')+'.gif">');
-	}
-	}
-	
-	} else {
-	return this.sendReply('Model not found.');
-	}
-	}
-	},
+
+        if (!target1) {
+            var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+            for (var x = 0; x < numbers.length; x++) {
+                if (target.indexOf('-' + numbers[i]) == -1) {
+                    return this.sendReply('|html|<img src = "http://www.pkparaiso.com/imagenes/xy/sprites/animados/' + target + '.gif">');
+                } else {
+                    return this.sendReply('|html|<img src = "http://play.pokemonshowdown.com/sprites/xyani/' + target + '.gif">');
+                }
+            }
+        } else {
+            if (toId(target1) === 'back') {
+                return this.sendReply('|html|<img src = "http://play.pokemonshowdown.com/sprites/xyani-back/' + target.toLowerCase().trim().replace(/ /g, '-') + '.gif">');
+            } else if (toId(target1) === 'shiny') {
+                return this.sendReply('|html|<img src = "http://play.pokemonshowdown.com/sprites/xyani-shiny/' + target.toLowerCase().trim().replace(/ /g, '-') + '.gif">');
+            } else {
+                this.sendReply(target1 + ' is not a valid parameter.');
+                var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+                for (var x = 0; x < numbers.length; x++) {
+                    if (target.indexOf('-' + numbers[i]) == -1) {
+                        return this.sendReply('|html|<img src = "http://www.pkparaiso.com/imagenes/xy/sprites/animados/' + target + '.gif">');
+                    } else {
+                        return this.sendReply('|html|<img src = "http://play.pokemonshowdown.com/sprites/xyani/' + target + '.gif">');
+                    }
+                }
+            }
+        }
+    },
 	
 	 clearall: function (target, room, user) {
         if (!this.can('clearall')) return;

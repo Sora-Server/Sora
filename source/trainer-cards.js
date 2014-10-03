11d0afb7acc00f6c64cd0ac5ccd2af145f344728
Loln8or <ancestraldog@hotmail.com>
2014-10-04 00:01:38 +1000

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -396,7 +396,7 @@ var trainerCards = {
 	ice: 'vescy',
 	vescy: function(target, room, user) {
 	        if (!this.canBroadcast()) return;
-		this.sendReplyBox('Gym Ldr <b>???</b><br />'+
+		this.sendReplyBox('Gym Ldr <b>Vescy</b><br />'+
 			  '<i>"The world is cold..."</i> <br />'+
 			  '<b>Type: <font color = 00e0ac>Ice</font></b><br />'+
 			  '<b>Ace:</b>  Kyruem-Black<br />');

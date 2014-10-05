a1d652c0d825e9ce870aa5088b4c0991ff9b6c5f
Loln8or <ancestraldog@hotmail.com>
2014-10-06 04:17:51 +1100

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -34,7 +34,7 @@ var trainerCards = {
 	 	      '<b>Battle Rules:</b> <br/>'+
 	 	      '-Ubers <br/>'+
 	 	      '<center><img src="http://i.imgur.com/pFtOL9I.png"> <img src="http://play.pokemonshowdown.com/sprites/xyani/yveltal.gif"></center> <br />'+
-	 	      '<img src="http://sprites.pokecheck.org/i/134.gif">'+
+	 	      '<img src="http://sprites.pokecheck.org/i/134.gif"> <br />'+
 	 	      'R.I.P. in piece Liquidbones 2013-2014 <br />'+
 	 	      'Gone, but definitely not forgotten. <br />');
 	},

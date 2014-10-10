1ba1c01414a27d5717f6cd1b6cd44a31a6442480
Loln8or <ancestraldog@hotmail.com>
2014-10-10 19:36:44 +1100

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -586,7 +586,7 @@ var trainerCards = {
 	leaderranks: 'ranks',
 		ranks: function(target, room, user) {
 			if (!this.canBroadcast()) return;
-			this.sendReplyBox('Listed here are the Top 10 Leaders in The Sora League based on performance in our Monthly Promotional Tournament with a direct addition from how many badge defends each Leader has achieved! Please keep in mind, the number of ranked Leaders may change month to month and the ranking methodology may be changed in future. <br />' +
+			this.sendReplyBox('Listed here are the Top 10 Leaders in The Sora League based on performance in our Monthly Promotional Tournament with a direct addition from how many badge defends each Leader has achieved! Please keep in mind, the number of ranked Leaders may change month to month and the ranking methodology may be changed in future. Please also note that the badge defend system isn\'t total career badge defends, but badge defends for each month. <br />' +
 				'-<b>1st <font color = 006b0a>Asch</font></b> (Grass)<br />'+
 				'-<b>2nd <font color = aa00ff>Srewop</font></b></b> (Poison)<br />'+
 				'-<b>3rd <font color = 00a6c4>Arsh</font></b> (Flying)<br />'+

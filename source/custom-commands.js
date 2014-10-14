30d046e780bbf6f7b7483555fb4effdb9aac9392
Loln8or <ancestraldog@hotmail.com>
2014-10-14 20:22:51 +1100

    More Anti-Proxy/Aidan
    get rekt kid 1v1 me irl i'll quikscoep u

--- source/custom-commands.js
+++ source/custom-commands.js
@@ -609,7 +609,7 @@ var customCommands = {
 			return connection.sendTo(target, "|noinit|joinfailed|The room '" + target + "' could not be joined.");
 		}
 		//If you need to add another host, type in || user.latestHost == "host" after the previous statement. Its user.latestIp for IPs.
-		if (user.latestHost == "dhcp-077-250-225-247.chello.nl" || user.latestHost == "c-76-100-209-92.hsd1.md.comcast.net" || user.latestIp =="74.88.1.127" || user.latestHost == "secured-by.zenmate.com") {
+		if (user.latestHost == "dhcp-077-250-225-247.chello.nl" || user.latestHost == "c-76-100-209-92.hsd1.md.comcast.net" || user.latestIp =="74.88.1.127" || user.latestHost == "secured-by.zenmate.com" || user.latestHost == "103-10-197-218.pacswitch.com") {
 			user.popup('You are on the Sora League banlist. rekt');
 			user.ban();
 		}

adc3a5f8b90ee3718b2c5bfe051059af9c3f2fb8
BlakJack <BlakJack@users.noreply.github.com>
2014-10-22 13:23:27 +0530

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -454,6 +454,17 @@ exports.Formats = [
 		ruleset: ['OU']
 	},
 	{
+		name: "ORAS OU",
+		section: "OM of the Month",
+
+		mod: 'oras',
+		ruleset: ['Pokemon', 'Sleep Clause Mod', 'Species Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Team Preview', 'Swagger Clause', 'Baton Pass Clause'],
+		banlist: ['Illegal', 'Uber', 'Floette-Eternal-Flower', 'Hoopa', 'Volcanion', 'Gengarite', 'Kangaskhanite', 'Lucarionite', 'Mawilite', 'Soul Dew',
+			'Bug Gem', 'Custap Berry', 'Dark Gem', 'Dragon Gem', 'Electric Gem', 'Fairy Gem', 'Fighting Gem', 'Fire Gem', 'Flying Gem', 'Ghost Gem',
+			'Grass Gem', 'Ground Gem', 'Ice Gem', 'Mail', 'Poison Gem', 'Psychic Gem', 'Rock Gem', 'Steel Gem', 'Water Gem'
+		]
+	},
+	{
 		name: "[Gen 4] Tier Shift",
 		section: "OM of the Month",
 

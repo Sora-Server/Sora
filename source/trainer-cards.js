a4b048bc496725c4cadb99947391cbd28ee966e5
Loln8or <ancestraldog@hotmail.com>
2015-01-24 20:41:45 +1100

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -492,13 +492,13 @@ var trainerCards = {
 			  
 	},
 	
-	
-	bug: function(target, room, user) {
+	bug: 'leaf',
+	leaf: function(target, room, user) {
 	        if (!this.canBroadcast()) return;
-		this.sendReplyBox('Gym Ldr <b>???</b><br />'+
-			  '<i>"???"</i> <br />'+
+		this.sendReplyBox('Gym Ldr <b>Leaf</b><br />'+
+			  '<i>"Bugs are legion, what can be crushed can be reborn; watch your step or you might be the one trodden on"</i> <br />'+
 			  '<b>Type: <font color = 65b510>Bug</font></b><br />'+
-			  '<b>Ace:</b> ??? <br />');
+			  '<b>Ace:</b> Scarab (Heracross) <br />');
         },
 	
 	ice: 'poppy',

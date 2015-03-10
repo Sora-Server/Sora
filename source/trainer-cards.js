0b135e2c6d2ed9f6083ed3ab2010905f64786139
Loln8or <ancestraldog@hotmail.com>
2015-03-10 21:17:08 +1100

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -420,15 +420,17 @@ var trainerCards = {
         },
         
         
-        
-	electric: function(target, room, user) {
+        electric: 'doge',
+	doge: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('Gym Ldr <b>???</b><br />'+
-			 '<i>"???"</i> <br />'+
+		this.sendReplyBox('Gym Ldr <b>Doge</b><br />'+
+			 '<i>"Electricity is our energy. Even when were out we generate more."</i> <br />'+
 		         '<b>Type: <font color = d6cc0c>Electric</font></b><br />'+
-			 '<b>Ace:</b> ???<br />');
+			 '<b>Ace:</b> Rotom-Fan<br />');
   
         },
+        
+        
         water: 'waffles',
 	waffles: function(target, room, user) {
 		if (!this.canBroadcast()) return;

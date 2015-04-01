5d6a950c1ed91f59f119632813a64d5020f1bb76
Loln8or <ancestraldog@hotmail.com>
2015-04-01 20:10:17 +1100

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -227,13 +227,13 @@ var trainerCards = {
         
       
         
-        
-        fighting: function(target, room, user) {
+        fighting: 'bulk',
+        bulk: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('Gym Ldr <b>???</b><br />'+
-			'<i>"???"</i> <br />'+
+		this.sendReplyBox('Gym Ldr <b>Bulk><br />'+
+			'<i>"Turn back or get a kick on your back."</i> <br />'+
 			'<b>Type: <font color = d83c08>Fighting</font></b><br />'+
-			'<b>Ace:</b> ??? <br />');
+			'<b>Ace:</b> Mega Gallade <br />');
         },
         
         

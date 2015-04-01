6a0f30b1124d7e6c707b596f863a02865a92bdee
Loln8or <ancestraldog@hotmail.com>
2015-04-01 18:23:03 +1100

    Update custom-commands.js

--- source/custom-commands.js
+++ source/custom-commands.js
@@ -1016,23 +1016,20 @@ var customCommands = {
 	
         },
         
-        dindins: function(target, room, user, connection) {
+       dindins: function(target, room, user, connection) {
 		if (!this.can('lock')) return false;
 		if (!user.isAway) {
 			var originalName = user.name;
 			var awayName = user.name + ' -Ⓓⓘⓝ ⓓⓘⓝⓢ';
 			delete Users.get(awayName);
 			user.forceRename(awayName, undefined, true);
-			this.add('|raw|-- <b><font color="#000000">' + originalName +'</font color></b> is now eating din dins. '+ (target ? " (" + target + ")" : ""));
+			this.add('|raw|-- <b><font color="#000000">' + originalName +'</font color></b> din dins. '+ (target ? " (" + target + ")" : ""));
 			user.isAway = true;
 		}
 		else {
-			return this.sendReply('You are already set as at din dins, type /back if you are now back');
+			return this.sendReply('You are already set as  at din dins, type /back if you are now back');
 		}
 		user.updateIdentity();
-	
-	
-        },
         
         
         

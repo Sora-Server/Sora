db7ccf3ad5e5a49a0e9fcb588efc93cb51d3b646
Loln8or <ancestraldog@hotmail.com>
2015-04-11 16:24:25 +1000

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -165,7 +165,7 @@ var trainerCards = {
 		'<b>Known for: </b>VoltTurn and Whimsistall shenanigans<br />'+
 		'<b>Achievements: </b>Ex-Elite Frontier, ex-Elite Four<br /><br />'+
 		'<center><img src="http://sprites.pokecheck.org/i/157.gif"><img src="http://sprites.pokecheck.org/i/530.gif"><img src="http://sprites.pokecheck.org/i/547.gif"><img src="http://sprites.pokecheck.org/t/144.gif"><img src="http://sprites.pokecheck.org/i/205.gif"><img src="http://sprites.pokecheck.org/i/310.gif"><img src="http://sprites.pokecheck.org/i/212.gif"></center> <br />'+
-		'<center><img src="http://havearandom.cu.cc/files/iero.png"; width="250" height="350"><br>Yellow is bae <3</center>');
+		'<center><img src="http://havearandom.cu.cc/files/iero.png"; width="100" height="140"><br>Yellow is bae <3 </center>');
 	
 	
         },

d3c98b849ca19b0d4006b5603a9215765c319c81
Loln8or <ancestraldog@hotmail.com>
2015-04-14 19:42:39 +1000

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -446,10 +446,10 @@ var trainerCards = {
         
 	water: function(target, room, user) {
 		if (!this.canBroadcast()) return;
-		this.sendReplyBox('Gym Ldr <b>ArthurZh</b><br />'+
-			 '<i>"The power of the seas, storms and rivers are mine to hold....and here you dare to stand before me?"</i> <br />'+
+		this.sendReplyBox('Gym Ldr <b>???</b><br />'+
+			 '<i>"???"</i> <br />'+
 		         '<b>Type: <font color = 0745ff>Water</font></b><br />'+
-			 '<b>Ace:</b> Mega Swampert<br />');
+			 '<b>Ace:</b> ???<br />');
 			 
 	},
         

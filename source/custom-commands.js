8ead51f0ed87de89a10ce9530351b86a21290fde
Siiilver <brahadeeshk@gmail.com>
2015-05-03 16:19:18 +0530

    Added stuff, including an avatar command.

--- source/custom-commands.js
+++ source/custom-commands.js
@@ -14,10 +14,9 @@ var customCommands = {
 	/*********************************************************
 	 * General commands
 	 *********************************************************/
-	
-		
 
     	sprite: function (target, room, user, connection, cmd) {
+    		if (!this.canBroadcast()) return;
 		if (!toId(target)) return this.sendReply('/sprite [Pokémon] - Allows you to view the sprite of a Pokémon');
 		target = target.toLowerCase().split(',');
 		var alt = '';
@@ -287,46 +286,27 @@ var customCommands = {
         this.popupReply('Administrators:\n--------------------\n' + buffer.admins + '\n\nLeaders:\n-------------------- \n' + buffer.leaders + '\n\nModerators:\n-------------------- \n' + buffer.mods + '\n\nDrivers:\n--------------------\n' + buffer.drivers + '\n\nVoices:\n-------------------- \n' + buffer.voices + '\n\n\t\t\t\tTotal Staff Members: ' + numStaff);
     },
 
-    regdate: function (target, room, user, connection) {
-        if (!this.canBroadcast()) return;
-        if (!target || target == "." || target == "," || target == "'") return this.parse('/help regdate');
-        var username = target;
-        target = target.replace(/\s+/g, '');
-
-        var options = {
-            host: "www.pokemonshowdown.com",
-            port: 80,
-            path: "/forum/~" + target
-        };
-
-        var content = "";
-        var self = this;
-        var req = http.request(options, function (res) {
-
-            res.setEncoding("utf8");
-            res.on("data", function (chunk) {
-                content += chunk;
-            });
-            res.on("end", function () {
-                content = content.split("<em");
-                if (content[1]) {
-                    content = content[1].split("</p>");
-                    if (content[0]) {
-                        content = content[0].split("</em>");
-                        if (content[1]) {
-                            regdate = content[1];
-                            data = username + ' was registered on' + regdate + '.';
-                        }
-                    }
-                } else {
-                    data = username + ' is not registered.';
-                }
-                self.sendReplyBox(data);
-                room.update();
-            });
-        });
-        req.end();
-    },
+    registered: 'regdate',
+	regdate: function (target, room, user, connection, cmd) {
+		if (!toId(target)) return this.sendReply("'" + target + "' is not a valid username.");
+		if (!toId(target).length > 18) return this.sendReply('Usernames can only contain 18 characters at the max.');
+		if (!this.canBroadcast()) return;
+		
+		var path = "http://pokemonshowdown.com/users/" + toId(target);
+		var self = this;
+		
+		request(path, function (error, response, body) {
+			if (error || response.statusCode === 404) {
+				self.sendReplyBox(target + ' is not registered.');
+				room.update();
+				return;
+			}
+			var date = body.split('<small>')[1].split('</small>')[0].substr(17);
+			if (!date) self.sendReplyBox(target + ' is not registered.');
+			else self.sendReplyBox(target + ' was registered on ' + date);
+			room.update();
+		});
+	},
 
     atm: 'profile',
     profile: function (target, room, user, connection, cmd) {
@@ -431,6 +411,9 @@ var customCommands = {
                     this.sendReply('You have purchased a custom symbol. You will have this until you log off for more than an hour. You may now use /customsymbol now.');
                     this.parse('/help customsymbol');
                     this.sendReply('If you do not want your custom symbol anymore, you may use /resetsymbol to go back to your old symbol.');
+                } else if (target.toLowerCase() === 'avatar') {
+                	user.canCustomAvatar = true;
+                	this.sendReply('You have purchased a custom avatar. Use /setavatar [URL] to set your custom avatar.');
                 } else {
                     this.sendReply('You have purchased ' + target + '. Please contact an admin to get ' + target + '.');
                     for (var u in Users.users) {
@@ -527,6 +510,29 @@ var customCommands = {
         this.sendReplyBox(Poll[room.id].display);
     },
 
+		setavatar: function (target, room, user, connection, cmd) {
+		if (!toId(target)) return this.sendReply('/setavatar URL - Sets a custom avatar.');
+		if (!user.canCustomAvatar && !this.can('hotpatch')) return false;
+		var avatarlist = JSON.parse(fs.readFileSync('config/customavatars.json'));
+		var formatList = ['png', 'jpg', 'gif', 'jpeg', 'bmp'];
+		var format = target.substr(-3);
+		if (formatList.indexOf(format) === -1) return this.sendReply('The format of your avatar is not supported. The allowed formats are ' + formatList.join(', ') + '.');
+		if (target.indexOf('https://') === 0) target = 'http://' + target.substr(8);
+		if (target.indexOf('http://') !== 0) target = 'http://' + target;
+
+		var self = this;
+		request.get(target).on('error', function () {
+			return self.sendReply("The avatar you picked doesn\'t exist. Try picking a new avatar.");
+		}).on('response', function (response) {
+			if (response.statusCode == 404) return self.sendReply("The avatar you picked is unavailable. Try picking a new avatar.");
+			avatarlist[user.userid] = user.userid + '.' + format;
+			fs.writeFile('config/customavatars.json', JSON.stringify(avatarlist, null, 1));
+			user.avatar = avatarlist[user.userid];
+			self.sendReply('|html|Your new avatar has been set to-<br/><img src = "' + target + '" width = 80 height = 80>');
+			user.canCustomAvatar = false;
+			response.pipe(fs.createWriteStream('config/avatars/' + user.userid + '.' + format));
+		});
+	},
 	
     customsymbol: function (target, room, user) {
         if (!user.canCustomSymbol) return this.sendReply('You need to buy this item from the shop to use.');
@@ -1054,13 +1060,12 @@ var customCommands = {
 	  endtour: function(target, room, user) {
 	  this.parse('/tour end');
 	  },
+
 	      u: 'urbandefine',
     ud: 'urbandefine',
     urbandefine: function (target, room, user) {
     	if (!this.canBroadcast()) return;
-    	if(room.id === 'lobby') {
-				return this.sendReply('|html|This command can only be used in the pub.');
-		}
+    	if (room.id === 'lobby' && this.broadcasting) return this.sendReply('You cannot broadcast this command in the lobby.')
         if (!target) return this.parse('/help urbandefine')
         if (target > 50) return this.sendReply('Phrase can not be longer than 50 characters.');
 
@@ -1101,9 +1106,6 @@ var customCommands = {
     def: 'define',
     define: function (target, room, user) {
     	if (!this.canBroadcast()) return;
-    	if(room.id === 'lobby') {
-				return this.sendReply('|html|This command can only be used in the pub.');
-		}
         if (!target) return this.parse('/help define');
         target = toId(target);
         if (target > 50) return this.sendReply('Word can not be longer than 50 characters.');

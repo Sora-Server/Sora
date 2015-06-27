7757dfc2a620e1f5df7d99fc1a61e73e20787fb1
Blazing360 <Rotflol360@gmail.com>
2015-06-27 20:30:01 +1000

    Sorry we ain't smogon

--- config/commands.js
+++ config/commands.js
@@ -1386,11 +1386,6 @@ var commands = exports.commands = {
 		);
 	},
 
-	staff: function (target, room, user) {
-		if (!this.canBroadcast()) return;
-		this.sendReplyBox("<a href=\"https://www.smogon.com/sim/staff_list\">Pokemon Showdown Staff List</a>");
-	},
-
 	avatars: function (target, room, user) {
 		if (!this.canBroadcast()) return;
 		this.sendReplyBox('You can <button name="avatars">change your avatar</button> by clicking on it in the <button name="openOptions"><i class="icon-cog"></i> Options</button> menu in the upper right. Custom avatars are only obtainable by staff.');
